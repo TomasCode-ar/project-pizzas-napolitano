5633a7d18c7ce72a410c482410825313d6dfcef7
tomas <tomasdesindes1@gmail.com>
2025-07-07 19:09:14 -0300

    Se agrega validación

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
-const countButtons=document.querySelectorAll("button").length;
-let products=[];
+const countButtons = document.querySelectorAll("button").length;
+let products = [];
 
 
-for(let i=0;i<countButtons;i++){
-document.querySelectorAll("button")[i].addEventListener("click",showValue);
+for (let i = 0; i < countButtons; i++) {
+    document.querySelectorAll("button")[i].addEventListener("click", showValue);
 }
 
-function showValue(){
-// Se hizo un click sobre el Button para comprar una Pizza//
+function showValue() {
+    // Se hizo un click sobre el Button para comprar una Pizza//
 
-products.push(this.value);
+    products.push(this.value);
 
-console.log(products);
+    console.log(products);
 
-//document.getElementById("badge").setAttribute("value",)
+    //document.getElementById("badge").setAttribute("value",)
+
+  //  console.log('Total: $' + totalPrice);
+   // document.getElementById("badge").setAttribute("value", products.length);
+   // document.getElementById("subtotal").style.display="block";
 
 }
