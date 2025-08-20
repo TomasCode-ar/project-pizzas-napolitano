e70c2d2251cf6fc9c440c2cd355224dba7671ade
tomas <tomasdesindes1@gmail.com>
2025-08-20 20:35:09 -0300

    -m "Se configura conexion"

--- login.php
+++ login.php
@@ -13,23 +13,14 @@
   session_start();
   if ($_SESSION['logueado'] = true) {
     include_once("config_login.php"); // ver usar require()
+    include_once("db.class.php");
     if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-      try {
-        $pdo = new PDO("mysql:host=" . SERVER_NAME . ";dbname=" . DATABASE_NAME, USER_NAME, PASSWORD);
-        // set the PDO error mode to exception
-        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected successfully";
-      } catch (PDOException $e) {
-        echo "Connection failed: " . $e->getMessage();
-      }
+      $link = new Db();
       $usr = $_POST['username'];
       $pass = $_POST['password'];
       $hashed_pass = hash('sha256', $pass);
       $sql = "select * from users where (username=? or email=?) and password=? and active='SI'";
-      // Use de sentencias prepared
-      // uso de POO- Programacion orientada a objetos
-      $stmt = $pdo->prepare($sql);
-      $stmt->execute([$usr, $usr, $hashed_pass]);
+      $stmt = $link->run($sql, [$usr, $usr, $hashed_pass]);
       $row = $stmt->fetch(PDO::FETCH_ASSOC);
       if (!$row) {
         echo "Los datos ingresados no son validos !";
