2119ca60e8152eb6d1c8118e201a63a9c6c8b6ae
tomas <tomasdesindes1@gmail.com>
2025-08-29 20:23:36 -0300

    -m "Se mejora la query de galeria"

--- index.php
+++ index.php
@@ -51,8 +51,8 @@
             include_once("config_products.php");
             include_once("db.class.php");
             $link = new Db();
-            $sql = "SELECT products.id_product, products.image, products.price, products.product_name, products.start_date, categories.category_name FROM products 
-INNER JOIN categories ON products.id_category = categories.id_category";
+            $sql = "select p.id_product,c.category_name,p.image,p.product_name,p.price, date_format(p.start_date,'%d/%m/%Y') as date from products p inner join categories c 
+on p.id_category=c.id_category order by c.category_name,p.price";
             $stmt = $link->run($sql);
             $data = $stmt->fetchAll();
             //recuperar un producto y llevarlo al li
@@ -64,7 +64,7 @@ INNER JOIN categories ON products.id_category = categories.id_category";
                             <figcaption>
                                 <h3><?php echo $row['product_name'] ?></h3>
                                 <p><?php echo $row['price'] ?></p>
-                                <time><?php echo $row['start_date'] ?></time>
+                                <time><?php echo $row['date'] ?></time>
                             </figcaption>
                         </figure>
                         <button class="button" value=<?php echo $row['id_product']  ?>" data-price="<?php echo $row['price']  ?>">Añadir al carrito
